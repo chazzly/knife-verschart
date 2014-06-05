374c0c7a1e55ebeda33778dd335d4df8997d16a5
Ruhl, Chaz <cruhl@cars.com>
2014-06-05 12:42:22 -0500

    Added Chef server to output, some clean-up

--- Verschart.rb
+++ Verschart.rb
@@ -11,6 +11,10 @@ module Verschart
 	#### Need to add variability - pull from envs.search (uncomment line 20).
 	#### --  Then how do I sort to my preference?
 	#### --  How do I designate a "PROD" which must be frozen?
+	srv = server_url.sub(/https:\/\//,'').sub(/:[0-9]*$/,'')
+	ui.info("Showing Versions for #{srv}")
+	ui.info("Version numbers show with '-f' are Frozen")
+	ui.info("")
 	hdrs= [ "Latest", "Sandbox", "Dev", "IT", "Staging", "PRODUCTION" ]
 
 	# Build environment hash containing all constraints.
@@ -19,7 +23,7 @@ module Verschart
 
 	charthash = Hash.new
 	search_envs.search('environment', qury ) do |enviro|
-#	  hdrs << enviro.name
+	# hdrs << enviro.name
 	  charthash[enviro.name] = Hash.new("X")
 	  enviro.cookbook_versions.each do | cb, v|
 	    charthash[enviro.name][cb] = v.to_s
@@ -30,33 +34,19 @@ module Verschart
 	  end
 	end 	
 
-	charthash['Latest'] = Hash.new
 	# Load list of latest cookbooks
+	charthash['Latest'] = Hash.new
 	server_side_cookbooks = Chef::CookbookVersion.latest_cookbooks
 	server_side_cookbooks.each do |svcb|
 	  mykb_name= svcb[0]
 	  fm = Chef::CookbookVersion.load(mykb_name, version="_latest")
-#	  ui.info("#{fm.metadata.name} -- #{fm.metadata.version}")
 	  charthash['Latest'][fm.metadata.name] = fm.metadata.version.to_s
 	  charthash['Latest'][fm.metadata.name] = charthash['Latest'][fm.metadata.name] + " -f" if fm.frozen_version?
 	end
-
-#	charthash.keys.each do | mynv |
-#	  ui.info("#{mynv}")
-#	end
-	
-#	hdrs.each do | myenv |
-#	  ui.info("#{myenv.class}")
-#	  ui.info("#{myenv}")
-#	  charthash[myenv].keys.each do | cbks |
-#	    vs = charthash[myenv][cbks]
-#	    ui.info("#{myenv} .. #{cbks} .. #{vs}")
-#	  end
-#	end
 	
 	#Set printf format string.  Add variability later (perhaps above when creating hdrs array.
 	pstring = "%-35s%-12s%-10s%-10s%-10s%-10s%-10s\n"
-	printf(pstring,"Cookbooks", *hdrs)
+	printf(pstring,"Cookbook", *hdrs)
 	charthash['Latest'].keys.each do |cb|
 	  cbout = [cb]
 	  hdrs.each do | env | 
