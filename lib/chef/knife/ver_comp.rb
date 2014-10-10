d9326e77f3eb4dd3d33da817dbc24262f8d03f39
Chaz Ruhl <cruhl@cars.com>
2014-10-10 15:58:10 -0500

    logic actually seems to work

--- lib/chef/knife/ver_comp.rb
+++ lib/chef/knife/ver_comp.rb
@@ -36,6 +36,18 @@ def const_valid(v_const)
   end
 end
 
+def pess_const(v_const)
+  c_op, c_vers = v_const.split(' ')
+  prts = c_vers.split('.')  
+  case prts.length
+  when 3
+    return [ prts[0], (prts[1].to_i + 1).to_s ,'0']
+  when 2
+  return [ (prts[0].to_i + 1).to_s ,'0','0']
+  end
+end
+  
+ 
 def ver_satisfies?(c_op, c_vers, v_vers)
   satisfy = false
   case c_op
@@ -50,6 +62,22 @@ def ver_satisfies?(c_op, c_vers, v_vers)
       if eval(tst)
         satisfy = true
         break
+      elsif v_vers[x] != c_vers[x]
+        break
+      end
+    end
+  when '<=','>='
+    if c_vers == v_vers 
+      satisfy = true
+    else
+      (0..2).each do |x| 
+        tst = v_vers[x] + c_op + c_vers[x]
+        if eval(tst) && v_vers[x] != c_vers[x]
+          satisfy = true
+          break
+        elsif v_vers[x] != c_vers[x]
+          break 
+        end
       end
     end
   end
@@ -62,6 +90,15 @@ def const_check(const, vers_to_check)
   if !c_check || !v_check
     return false
   end
-  
-  return ver_satisfies?(c_check[1], c_check[2], v_check[1])
+  if c_check[1] == '~>'
+    lwlimit = ver_satisfies?('>=', c_check[2], v_check[1]) 
+    uplimit = ver_satisfies?('<',  pess_const(const), v_check[1])
+    if lwlimit && uplimit
+      return true
+    else
+      return false
+    end 
+  else 
+    return ver_satisfies?(c_check[1], c_check[2], v_check[1])
+  end
 end
